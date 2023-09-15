cd780f1cc1df2817903711f0fe2b55799aeda7ea
paulboutros <paulboutros436@hotmail.com>
2023-09-15 10:28:08 +0300

    cosol log port=

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
+console.log(`Server is running on port: ${process.env.PORT}`);
+
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;

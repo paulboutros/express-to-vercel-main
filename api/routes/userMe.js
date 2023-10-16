66c5be1ec70110b9c28c1f3a09dea2e94c08d578
paulboutros <paulboutros436@hotmail.com>
2023-10-16 21:40:27 +0300

    user/me header A-C-A-O

--- api/routes/userMe.js
+++ api/routes/userMe.js
@@ -10,11 +10,19 @@ const router = express.Router();
   // do not forget to use the endpoint in index.js
 router.get("/user/me", async (req, response) => {
   
+  /*
  const requestBody = req.state.user;
+  response.json(  requestBody  );
+ */
 
- 
- response.json(  requestBody  );
- 
+  res.header('Access-Control-Allow-Origin', 'https://wuli.rocks');
+  // Your code to handle the "user/me" endpoint...
+  const requestBody = req.state.user;
+  
+  // Send your response with the specified headers
+  res.json(requestBody);
+
+
 
 })
 export default router;
